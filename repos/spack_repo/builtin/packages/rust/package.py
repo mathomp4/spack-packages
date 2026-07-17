93e1aa4033b73b32c6e18e3a95ea7b2210b2deb1
Matt Thompson <fortran@gmail.com>
2026-07-17 13:39:53 -0400

    Update repos/spack_repo/builtin/packages/rust/package.py
    Co-authored-by: Alec Scott <hi@alecbcs.com>

--- repos/spack_repo/builtin/packages/rust/package.py
+++ repos/spack_repo/builtin/packages/rust/package.py
@@ -208,9 +208,7 @@ class Rust(Package):
         # Otherwise retain Spack's isolated per-package cache.
         cargo_home = os.environ.get("CARGO_HOME")
         cargo_offline = os.environ.get("CARGO_NET_OFFLINE", "").lower() == "true"
-        if cargo_home and cargo_offline:
-            env.set("CARGO_HOME", cargo_home)
-        else:
+        if not (cargo_home and cargo_offline):
             env.set("CARGO_HOME", join_path(dependent_spec.package.stage.path, "cargo"))
 
         # Until we get a little more integration with cargo or offload solving to spack
